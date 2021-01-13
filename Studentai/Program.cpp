da42816baf854179f8cbedcdad39ef18ee8b4506
salvija.racka@gmail.com <salvija.racka@gmail.com>
2021-01-13 12:05:06 +0200

    pridetas diegimo failas

--- Studentai/Program.cpp
+++ Studentai/Program.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include "Studentai_Class.h"
-#include <list>
+#include "Studentas_Class.h"
 
 using namespace std;
 
 int main()
 {
-	list<int> grades = { 5, 6, 7, 8 };
-	Studentas_Class studentas("Vardenis", "Pavardenis", 8, grades);
+    srand(time(0));
 
-	cout << studentas.getFullName() << endl;
+    Studentai_Class studentai;
+    Studentas_Class studentas;
 
-	return 0;
+    int n;
+    cout << "Iveskite studentu skaicius: " << endl;
+    cin >> n;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        studentas = Studentas_Class();
+        studentas.set_name("Vardenis" + i);
+        studentas.set_surname("Pavardenis" + i);
+        studentas.set_egzamResult(rand() % 10 + 1);
+        studentas.randomGradesGenerator();
+        studentas.finalGrade("vid");
+        studentai.get_studentai().push_back(studentas);
+    }
+
+    return 0;
 }
